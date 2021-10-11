f02ab28b869633c23398e0cfb22c2b564c6e5e8e
[daniel] <[danieruzeta@gmail.com]>
2021-10-11 16:27:03 -0300

    add index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+ 
+// Show errors during initialization and execution
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+	
+require "Config/Autoload.php";
+require "Config/Config.php";
+
+use Config\Autoload as Autoload;
+use Config\Router 	as Router;
+use Config\Request 	as Request;
+		
+Autoload::start();
+
+// Needed to maintain a session
+session_start();
+
+require_once(VIEWS_PATH."header.php");
+
+// Use of the framework
+Router::Route(new Request());
+
+require_once(VIEWS_PATH."footer.php");
+
+?>
