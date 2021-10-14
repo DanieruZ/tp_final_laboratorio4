d2a334b09db3671a0764e2c753ecc296a5a0f400
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2021-10-14 16:53:56 -0300

    Change Form

--- Views/student-add.php
+++ Views/student-add.php
@@ -1,33 +1,68 @@
-<?php
-    require_once('nav.php');
-?>
+<?php require_once('nav.php'); ?>
+
 <main class="py-5">
-     <section id="listado" class="mb-5">
-          <div class="container">
-               <h2 class="mb-4">Agregar alumno</h2>
-               <form action="<?php echo FRONT_ROOT ?>Student/Add" method="post" class="bg-light-alpha p-5">
-                    <div class="row">                         
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Legajo</label>
-                                   <input type="text" name="recordId" value="" class="form-control">
-                              </div>
-                         </div>
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Nombre</label>
-                                   <input type="text" name="firstName" value="" class="form-control">
-                              </div>
-                         </div>
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Apellido</label>
-                                   <input type="text" name="lastName" value="" class="form-control">
-                              </div>
-                         </div>
-                    </div>
-                    <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Agregar</button>
-               </form>
-          </div>
-     </section>
+	<section id="listado" class="mb-5">
+		<div class="container">
+			<h2 class="mb-4">Agregar alumno</h2>
+			<form action="<?php echo FRONT_ROOT ?>student/add" method="post" class="bg-light-alpha p-5">
+				<div class="row">
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="careerId">Career ID</label>
+							<input type="text" name="careerId" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="firstName">First Name</label>
+							<input type="text" name="firstName" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="lastName">Last Name</label>
+							<input type="text" name="lastName" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="dni">DNI</label>
+							<input type="text" name="dni" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="fileNumber">File Number</label>
+							<input type="text" name="fileNumber" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="gender">Gender</label>
+							<input type="text" name="gender" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="birthDate">Birthdate</label>
+							<input type="text" name="birthDate" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="email">Email</label>
+							<input type="email" name="email" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="form-group">
+							<label for="phoneNumber">Phone Number</label>
+							<input type="text" name="phoneNumber" class="form-control">
+						</div>
+					</div>
+				</div>
+				<button type="submit" name="button" class="btn btn-dark ml-auto d-block">Agregar</button>
+			</form>
+		</div>
+	</section>
 </main>
