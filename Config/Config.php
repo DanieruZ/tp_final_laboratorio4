82b1ef7ee1e692ee2fe1733bfb4ada3b5b615a82
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2021-10-14 16:29:14 -0300

    Add Config

---
+++ Config/Config.php
@@ -0,0 +1,13 @@
+<?php namespace Config;
+
+define("ROOT", dirname(__DIR__) . "/");
+// IMPORTANT Path to your project's root folder
+define("FRONT_ROOT", "/tp_final_laboratorio4/");
+define("VIEWS_PATH", "Views/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
+define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+// student's api key
+define('API_KEY','4f3bceed-50ba-4461-a910-518598664c08');
+define('API_URL','https://utn-students-api.herokuapp.com/api/Student');
+?>
+
