4be63e8218b95fa0cf1dca717b2a286430d06cac
JeremiasRoca <jereroca2@gmail.com>
2021-10-15 13:23:00 -0300

    add company-add

---
+++ Views/company-add.php
@@ -0,0 +1,83 @@
+<?php
+
+?>
+<head>
+     <title>New Company</title>
+</head>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Add Company</h2>
+               <form action="<?php echo FRONT_ROOT ?>Company/ShowAddView" method="post" class="bg-light-alpha p-5">
+                    <div class="col">                         
+                         <div class="col-lg-4">
+                              
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="CompanyId">CompanyId</label>
+                                   <input type="number" name="companyId" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="companyName">companyName</label>
+                                   <input type="text" name="companyName" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="email">email</label>
+                                   <input type="email" name="email" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="phoneNumber">phoneNumber</label>
+                                   <input type="text" name="phoneNumber" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="address">address</label>
+                                   <input type="text" name="address" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="city">city</label>
+                                   <input type="text" name="city" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="country">country</label>
+                                   <input type="text" name="country" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="totalEmployees">totalEmployees</label>
+                                   <input type="text" name="totalEmployees" value="" class="form-control" Required>
+                              </div>
+                         </div>
+
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="companyInfo">companyInfo</label>
+                                   <input type="text" name="companyInfo" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="active">active</label>
+                                   <input type="text" name="active" value="" class="form-control" Required>
+                              </div>
+                         </div>
+                    </div>
+                    <button type="submit" class="btn btn-dark ml-auto d-block">Add</button>
+                    <a href="<?php echo FRONT_ROOT ?>Admin">Back to Main</a>
+               </form>
+          </div>
+     </section>
+</main
