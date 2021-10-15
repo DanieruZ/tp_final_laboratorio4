5645f92cbfffa8e6e882911e51d3fbe3c16dbafa
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2021-10-15 20:14:00 -0300

    fix to add Student

--- Controllers/StudentController.php
+++ Controllers/StudentController.php
@@ -34,8 +34,8 @@
 
         public function AddStudent($careerId, $firstName, $lastName, $dni, $fileNumber, $gender, $birthDate, $email, $phoneNumber, $active)
         {
-            //$studentId = $this->studentDAO->getNextId();
-            $student = new Student($careerId, $firstName, $lastName, $dni, $fileNumber, $gender, $birthDate, $email, $phoneNumber, $active);
+            $studentId = $this->studentDAO->getNextId();
+            $student = new Student($studentId,$careerId, $firstName, $lastName, $dni, $fileNumber, $gender, $birthDate, $email, $phoneNumber, $active);
             $this->studentDAO->addStudent($student);
 
             $this->ShowAddView();
