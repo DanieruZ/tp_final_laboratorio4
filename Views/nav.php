7be2968d0872054a596b92894b11427f0f5c1efb
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2021-10-15 20:49:57 -0300

    fix student nav

--- Views/nav.php
+++ Views/nav.php
@@ -1,13 +1,16 @@
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
      <span class="navbar-text">
-          <strong>Administrador</strong>
+          <strong>Student</strong>
      </span>
+   
      <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Agregar Alumno</a>
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowListViewAdmin">List Company</a>
           </li>
           <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowListView">Listar Alumnos</a>
-          </li>          
+               <a class="nav-link active" aria-current="page" href="<?php echo FRONT_ROOT ?>Login/Logout">Logout</a>
+          </li> 
+                   
      </ul>
+     
 </nav>
