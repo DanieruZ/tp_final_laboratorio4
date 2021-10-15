485eeda8be43789081f067b0146b1654104e9818
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2021-10-15 17:29:47 -0300

    add button delete and href

--- Views/company-list-admin.php
+++ Views/company-list-admin.php
@@ -1,5 +1,5 @@
 <?php
-     require_once "nav-admin.php";
+require_once "nav-admin.php";
 ?>
 <main class="py-5">
      <section id="listado" class="mb-5">
@@ -17,35 +17,36 @@
                          <th>totalEmployees </th>
                          <th>companyInfo </th>
                          <th>active </th>
-                        
+
+
                     </thead>
                     <tbody>
                          <?php
-                              foreach($companyList as $company)
-                              {
-                                   ?>
-                                        <tr>
-                                             <td><?php echo $company->getCompanyId(); ?></td>                                                
-                                             <td><?php echo $company->getCompanyName(); ?></td>
-                                             <td><?php echo $company->getEmail(); ?></td>
-                                             <td><?php echo $company->getPhoneNumber(); ?></td>                                                
-                                             <td><?php echo $company->getAddress(); ?></td>
-                                             <td><?php echo $company->getCity(); ?></td>
-                                             <td><?php echo $company->getCountry(); ?></td>                                                
-                                             <td><?php echo $company->getTotalEmployees(); ?></td>
-                                             <td><?php echo $company->getCompanyInfo(); ?></td>
-                                             <td><?php echo $company->getActive(); ?></td>                                                
-
-                                             <td>
-                                                
-                                             </td>
-                                        </tr>
-                                   <?php
-                              }
+                         foreach ($companyList as $company) {
                          ?>
-                         </tr>
+                              <tr>
+                                   <td><?php echo $company->getCompanyId(); ?></td>
+                                   <td><?php echo $company->getCompanyName(); ?></td>
+                                   <td><?php echo $company->getEmail(); ?></td>
+                                   <td><?php echo $company->getPhoneNumber(); ?></td>
+                                   <td><?php echo $company->getAddress(); ?></td>
+                                   <td><?php echo $company->getCity(); ?></td>
+                                   <td><?php echo $company->getCountry(); ?></td>
+                                   <td><?php echo $company->getTotalEmployees(); ?></td>
+                                   <td><?php echo $company->getCompanyInfo(); ?></td>
+                                   <td><?php echo $company->getActive(); ?></td>
+                                   <td>
+                                        
+                                        <button type="submit" name="btnRemove"  class="btn btn-danger">
+                                        <a href= "<?php echo FRONT_ROOT ?>Company/DeleteCompany?$id=<?php echo $company->getCompanyId(); ?>">Delete</a>
+                                        </button>
+                                   </td>
+                              </tr>
+                         <?php
+                         }
+                         ?>
                     </tbody>
-                    
+
                </table>
           </div>
 
