e6934c4716a78d4915df9a741d2861d005a33bbc
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2021-10-15 01:25:56 -0300

    add loginController

---
+++ Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php 
+    namespace Controllers;
+
+    use DAO\StudentDAO as StudentDAO;
+    use Models\Student as Student;
+    use Controllers\HomeController as HomeController;
+
+    class LoginController{
+        public function Login($email){
+           
+            $studentRepo = new StudentDAO();
+            $studentList = $studentRepo->getAllStudent();
+            if($email == "admin@myapp.com"){
+                var_dump($email);
+                header("location:".FRONT_ROOT."Admin");
+            }
+            $student = $studentRepo->getStudentByEmail($email);
+
+            if($student){
+                $_SESSION["student"]=$student;
+                header("location:".FRONT_ROOT."Student");           
+            }else{
+                $incorrect = new HomeController();
+                $incorrect->index("Error al ingresar el email");
+            }             
+        }
+    public function Logout(){
+        header("location: ../index.php");
+    }  
+}
+
+?>
