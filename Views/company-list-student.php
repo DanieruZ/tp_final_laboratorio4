dbc04855b0800940a699023fc89864f3d9772b7b
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2021-10-15 16:16:38 -0300

    fix Company List Student

--- Views/company-list-student.php
+++ Views/company-list-student.php
@@ -1,37 +1,56 @@
-<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-     <span class="navbar-text"> List Company </span>
-     <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student "> Back to Main</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Login/Logout" >logout</a>
-          </li>
-     </ul>
-</nav>
+<?php
+    require_once('nav-admin.php');
+?>
 <main class="py-5">
-     <section id="listado" class="mb-5">
-          <div class="container">
-               <h2 class="mb-4">List Company</h2>
-               <table class="table bg-light-alpha">
-                    <thead>
-                         <th>CompanyId</th>
-                         <th>companyName</th>
-                         <th>email</th>
-                         <th>phoneNumber</th>
-                         <th>address</th>
-                         <th>city</th>
-                         <th>country</th>
-                         <th>totalEmployees</th>
-                         <th>companyInfo</th>
-                         <th>active</th>
-                        
+	<section id="listado" class="mb-5">
+		<div class="container">
+			<h2 class="mb-4">STUDENT'S LIST</h2>
 
-                    </thead>
-                   
-                    
-               </table>
-          </div>
-
-     </section>
+			<table class="table bg-light">
+				<thead class="bg-dark text-white">
+					<th>Student ID</th>
+					<th>Career ID</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>DNI</th>
+					<th>File Number</th>
+					<th>Gender</th>
+					<th>Birthdate</th>
+					<th>Email</th>
+					<th>Phone Number</th>
+					<th>Active</th>
+				</thead>
+				<tbody>
+					<form action="Process/student-remove.php" method="post">
+						<?php
+						if (isset($studentList)) {
+							foreach ($studentList as $student) {
+						?>
+								<tr>
+									<td><?php echo $student->getStudentId(); ?></td>
+									<td><?php echo $student->getCareerId(); ?></td>
+									<td><?php echo $student->getFirstName(); ?></td>
+									<td><?php echo $student->getLastName(); ?></td>
+									<td><?php echo $student->getDni(); ?></td>
+									<td><?php echo $student->getFileNumber(); ?></td>
+									<td><?php echo $student->getGender(); ?></td>
+									<td><?php echo $student->getBirthDate(); ?></td>
+									<td><?php echo $student->getEmail(); ?></td>
+									<td><?php echo $student->getPhoneNumber(); ?></td>
+									<td><?php echo $student->getActive(); ?></td>
+									<td>
+										<button type="submit" name="btnRemove" class="btn btn-danger" value="<?php echo $student->getDni(); ?>">Eliminar</button>
+									</td>
+								</tr>
+						<?php
+							}
+						}
+						?>
+					</form>
+				</tbody>
+			</table>
+		</div>
+	</section>
 </main>
+
+<?php include('footer.php') ?>
