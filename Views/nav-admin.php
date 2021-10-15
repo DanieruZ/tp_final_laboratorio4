aa7bc97015fe12e1e1154607c0c0a5079e8cb134
JeremiasRoca <jereroca2@gmail.com>
2021-10-15 13:26:45 -0300

    add nav.admin

---
+++ Views/nav-admin.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     <span class="navbar-text">
+          <strong>Framework</strong>
+     </span>
+     <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Admin">home</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowAddView">Add Company</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT?>Company\ShowListViewAdmin" >List Company</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" readonly href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Add Student</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowListView">List Student</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="<?php echo FRONT_ROOT ?>login.php">Logout</a>
+          </li>          
+     </ul>
+</nav>
