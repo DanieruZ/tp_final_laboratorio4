1ae754950d1a61338aad33e496162e156080c2ad
JeremiasRoca <jereroca2@gmail.com>
2021-10-15 13:23:37 -0300

    add company-list.admin

---
+++ Views/company-list-admin.php
@@ -0,0 +1,46 @@
+<?php
+     require_once "nav-admin.php";
+?>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Listado de Empresas</h2>
+               <table class="table bg-light-alpha">
+                    <thead>
+                         <th>companyId</th>
+                         <th>companyName</th>
+                         <th>email</th>
+                         <th>phoneNumber</th>
+                         <th>address</th>
+                         <th>city</th>
+                         <th>country </th>
+                         <th>totalEmployees </th>
+                         <th>companyInfo </th>
+                         <th>active </th>
+                        
+                    </thead>
+                    <tbody>
+                         <?php
+                              foreach($companyList as $company)
+                              {
+                                   ?>
+                                        <tr>
+                                             <td><?php echo $company->getCompanyId(); ?></td>
+                                                
+                                             <td><?php echo $company->getTotalEmployees(); ?></td>
+                                             <td><?php echo $company->getCompanyInfo(); ?></td>
+                                             <td>
+                                                
+                                             </td>
+                                        </tr>
+                                   <?php
+                              }
+                         ?>
+                         </tr>
+                    </tbody>
+                    
+               </table>
+          </div>
+
+     </section>
+</main>
