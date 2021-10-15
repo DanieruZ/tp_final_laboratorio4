69b1a57d14a5299b5b18ef972fbacc42f3c827a5
JeremiasRoca <jereroca2@gmail.com>
2021-10-15 13:24:12 -0300

    add company-list.student

---
+++ Views/company-list-student.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     <span class="navbar-text"> List Company </span>
+     <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student "> Back to Main</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Login/Logout" >logout</a>
+          </li>
+     </ul>
+</nav>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">List Company</h2>
+               <table class="table bg-light-alpha">
+                    <thead>
+                         <th>CompanyId</th>
+                         <th>companyName</th>
+                         <th>email</th>
+                         <th>phoneNumber</th>
+                         <th>address</th>
+                         <th>city</th>
+                         <th>country</th>
+                         <th>totalEmployees</th>
+                         <th>companyInfo</th>
+                         <th>active</th>
+                        
+
+                    </thead>
+                   
+                    
+               </table>
+          </div>
+
+     </section>
+</main>
