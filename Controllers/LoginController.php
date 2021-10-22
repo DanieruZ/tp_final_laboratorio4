b4d4d8a569d722176f5fbf61a2e18038c5b2e590
DanieruZ <[danieruzeta@gmail.com]>
2021-10-22 20:18:51 -0300

    fix temp login

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -1,33 +1,44 @@
 <?php 
-    namespace Controllers;
 
-    use DAO\StudentDAO as StudentDAO;
-    use Models\Student as Student;
-    use Controllers\HomeController as HomeController;
+namespace Controllers;
 
-    class LoginController{
-        public function Login($email){
-           
-            $studentRepo = new StudentDAO();
-            $studentList = $studentRepo->getAllStudent();
-            if($email == "admin@myapp.com"){
-                //var_dump($email);
-                header("location:".FRONT_ROOT."Admin");
-            }
-            $student = $studentRepo->getStudentByEmail($email);
+use DAO\StudentDAO as StudentDAO;
+use Models\Student as Student;
+use Controllers\HomeController as HomeController;
 
-            if($student){
-                $_SESSION["student"]=$student;
-                header("location:".FRONT_ROOT."Student");           
-            }else{
-                $incorrect = new HomeController();
-                $incorrect->index("Error al ingresar el email");
-            }             
-        }
-    public function Logout(){
-        header("location: ../index.php");
+
+/**
+ * * instalar extension Better Comments para los comentarios
+ * ! aclaracion: no le pego a la api ni al json, es para acomodar las opciones en las sesiones.
+ * ! los email estan predefinidos, si no loguea se redirecciona al index.
+ * 
+ */
+class LoginController {
+
+  public function Login($email) {
+    // $studentRepo = new StudentDAO();
+    // $studentList = $studentRepo->getAllStudent();
+    
+    if($email == 'admin@myapp.com') {
+      header("location:".FRONT_ROOT."Admin");
+    }
+
+    if($email == 'wlorant1@sbwire.com') {
+      // $_SESSION["student"] = $student;
+      header("location:".FRONT_ROOT."Student");         
+    }
+
+    if($email != 'admin@myapp.com' || $email != 'wlorant1@sbwire.com') {
+      $incorrect = new HomeController();
+      $incorrect->Index("Error al ingresar el email");
+    }             
+  }
+  
+  public function Logout(){
+    header("location: ../index.php");
       
-    }  
+  }  
+
 }
 
 ?>
