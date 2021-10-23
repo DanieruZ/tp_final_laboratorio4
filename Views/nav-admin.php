2051efa1898ce5a1ca3f5efcceaa878af59705f2
DanieruZ <[danieruzeta@gmail.com]>
2021-10-23 02:31:47 -0300

    fix nav-admin

--- Views/nav-admin.php
+++ Views/nav-admin.php
@@ -1,25 +1,13 @@
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-     <span class="navbar-text">
-          <strong>Admin</strong>
-     </span>
-     <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Admin">home</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowAddView">Add Company</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT?>Company\ShowListViewAdmin" >List Company</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" readonly href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Add Student</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowListViewStudent">List Student</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link active" aria-current="page" href="<?php echo FRONT_ROOT ?>Login/Logout">Logout</a>
-          </li>          
-     </ul>
+  <span class="navbar-text">
+    <strong>Admin</strong>
+  </span>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Admin">Home</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowAddView">Add Company</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowAdminListView" >Company's List</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Add Student</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAdminListView">Student's List</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Login/Logout">Logout</a> </li>  
+  </ul>
 </nav>
