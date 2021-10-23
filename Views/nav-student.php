578d28dbb3b6d4db675c1d4dc63cf26d99585c9a
DanieruZ <[danieruzeta@gmail.com]>
2021-10-23 02:31:08 -0300

    add nav-student

---
+++ Views/nav-student.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <span class="navbar-text">
+    <strong>Student</strong>
+  </span>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student">Home</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowStudentListView">Company's List</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowStudentListView">Student's List</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Login/Logout">Logout</a> </li>  
+  </ul>
+</nav>
