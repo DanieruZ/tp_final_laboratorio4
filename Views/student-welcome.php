ca8a9cc1bcacc42c07154111ea6c23dba6ebf3e9
DanieruZ <[danieruzeta@gmail.com]>
2021-10-23 02:26:53 -0300

    add admin-welcome & student-welcome

---
+++ Views/student-welcome.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Views;
+
+use Models\Student as Student;
+use DAO\StudentDAO as StudentDAO;
+
+/**
+ * ! esto no funca
+ * * aca datos del estudiante & estado academico 
+ * * el model AcademicStatus se tiene que crear en Models
+ */
+
+?>
+<main class="py-5">
+    <section id="listado" class="mb-5">
+        <div class="container-fluid">
+            <title><?php echo $student->getFirstName() . " " . $student->getLastName(); ?></title>
+            
+            </head>
+            <body>
+            <h1>Aca los datos de alumno & su estado academico</h1>
+                <h1><?php echo $student->getFirstName() . " " . $student->getLastName(); ?></h1>
+            </body>
+            <table class="table bg-light">
+                <thead class="bg-dark text-white">
+                    <tr>
+                        <th>Student ID</th>
+                        <th>Career ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>DNI</th>
+                        <th>File Number</th>
+                        <th>Gender</th>
+                        <th>Birthdate</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Active</th>
+                    </tr>
+
+                </thead>
+                <tbody>
+
+                    <?php
+                    $studentRepo = new StudentDAO();
+
+                    $studentList = $studentRepo->getAllStudent();
+                    $email = "wlorant1@sbwire.com";
+
+                    $student = $studentRepo->getStudentByEmail($email);
+
+                    ?>
+
+                    <tr>
+                        <td><?php echo $student->getStudentId(); ?></td>
+                        <td><?php echo $student->getCareerId(); ?></td>
+                        <td><?php echo $student->getFirstName(); ?></td>
+                        <td><?php echo $student->getLastName(); ?></td>
+                        <td><?php echo $student->getDni(); ?></td>
+                        <td><?php echo $student->getFileNumber(); ?></td>
+                        <td><?php echo $student->getGender(); ?></td>
+                        <td><?php echo $student->getBirthDate(); ?></td>
+                        <td><?php echo $student->getEmail(); ?></td>
+                        <td><?php echo $student->getPhoneNumber(); ?></td>
+                        <td><?php echo $student->getActive(); ?></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </section>
+</main>
+
+<?php include('footer.php') ?>
