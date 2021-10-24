34ded8925ab02e25af6ab9540b0a925f1384c524
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-10-24 18:21:09 -0300

    fix login

--- Views/login.php
+++ Views/login.php
@@ -1,18 +1,15 @@
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
-            
-
-
-
                <?php
-             
-             use Models\Student as Student;
-             $studentList = array();
+
+               use Models\Student as Student;
+
+               $studentList = array();
 
 
                $apiStudent = curl_init(API_URL . 'Student');
-               curl_setopt($apiStudent,CURLOPT_URL, API_URL);
+               curl_setopt($apiStudent, CURLOPT_URL, API_URL);
 
                curl_setopt($apiStudent, CURLOPT_HTTPHEADER, array('x-api-key: ' . API_KEY));
                curl_setopt($apiStudent, CURLOPT_RETURNTRANSFER, true);
@@ -22,82 +19,74 @@
 
                //die(var_dump($response));
 
-               
-               $arrayToDecode = ($response) ? json_decode($response, true) : array();
-         
-               
 
-                foreach($arrayToDecode as $valuesArray) {
+               $arrayToDecode = ($response) ? json_decode($response, true) : array();
+
+
+
+               foreach ($arrayToDecode as $valuesArray) {
                     $student = new Student(
-                    $valuesArray["studentId"], 
-                    $valuesArray["careerId"], 
-                    $valuesArray["firstName"], 
-                    $valuesArray["lastName"], 
-                    $valuesArray["dni"], 
-                    $valuesArray["fileNumber"], 
-                    $valuesArray["gender"], 
-                    $valuesArray["birthDate"], 
-                    $valuesArray["email"], 
-                    $valuesArray["phoneNumber"], 
-                    $valuesArray["active"]);
-                    
+                         $valuesArray["studentId"],
+                         $valuesArray["careerId"],
+                         $valuesArray["firstName"],
+                         $valuesArray["lastName"],
+                         $valuesArray["dni"],
+                         $valuesArray["fileNumber"],
+                         $valuesArray["gender"],
+                         $valuesArray["birthDate"],
+                         $valuesArray["email"],
+                         $valuesArray["phoneNumber"],
+                         $valuesArray["active"]
+                    );
+
                     array_push($this->studentList, $student);
                }
 
-                   // var_dump($this->studentList);
+               // var_dump($this->studentList);
 
                $arrayToEncode = array();
-    
-        foreach($this->studentList as $student){
-          $valuesArray["studentId"] = $student->getStudentId();
-          $valuesArray["careerId"] = $student->getCareerId();
-          $valuesArray["firstName"] = $student->getFirstName();
-          $valuesArray["lastName"] = $student->getLastName();
-          $valuesArray["dni"] = $student->getDni();
-          $valuesArray["fileNumber"] = $student->getFileNumber();
-          $valuesArray["gender"] = $student->getGender();
-          $valuesArray["birthDate"] = $student->getBirthDate();
-          $valuesArray["email"] = $student->getEmail();
-          $valuesArray["phoneNumber"] = $student->getPhoneNumber();
-          $valuesArray["active"] = $student->getActive();
-    
-          array_push($arrayToEncode, $valuesArray);
-          }
-          $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
-        file_put_contents('Data/students.json', $jsonContent);
+
+               foreach ($this->studentList as $student) {
+                    $valuesArray["studentId"] = $student->getStudentId();
+                    $valuesArray["careerId"] = $student->getCareerId();
+                    $valuesArray["firstName"] = $student->getFirstName();
+                    $valuesArray["lastName"] = $student->getLastName();
+                    $valuesArray["dni"] = $student->getDni();
+                    $valuesArray["fileNumber"] = $student->getFileNumber();
+                    $valuesArray["gender"] = $student->getGender();
+                    $valuesArray["birthDate"] = $student->getBirthDate();
+                    $valuesArray["email"] = $student->getEmail();
+                    $valuesArray["phoneNumber"] = $student->getPhoneNumber();
+                    $valuesArray["active"] = $student->getActive();
+
+                    array_push($arrayToEncode, $valuesArray);
+               }
+               $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
+               file_put_contents('Data/students.json', $jsonContent);
                //var_dump($arrayToEncode);
-             
+
                ?>
-               
-               <?php
+
+               <main class="d-flex align-items-center justify-content-center height-100">
+                    <div class="content">
+                         <header class="text-center">
+                              <h2>Welcome</h2>
+                              <h2>Prueba merge</h2>
+                         </header>
+                         <form action="Login/Login" method="post" class="login-form bg-light p-5 text-white">
+                              <div class="form-group">
+                                   <label for="email"></label>
+                                   <input type="email" name="email" class="form-control form-control-lg" placeholder="Enter your Email" required>
+                              </div>
 
 
-
-
-
-?>
-
-
-     <main class="d-flex align-items-center justify-content-center height-100">
-          <div class="content">
-               <header class="text-center">
-                    <h2>Welcome</h2>
-                    <h2>Prueba merge</h2>
-               </header>
-               <form action="Login/Login" method="post" class="login-form bg-light p-5 text-white">
-                    <div class="form-group">
-                         <label for="email"></label>
-                         <input type="email" name="email" class="form-control form-control-lg" placeholder="Enter your Email" required>
+                              <button class="btn btn-dark btn-block btn-lg" type="submit">Session Start</button>
+                         </form>
                     </div>
-                  
-                    
-                    <button class="btn btn-dark btn-block btn-lg" type="submit">Session Start</button>
-               </form>
-          </div>
-     </main>
+               </main>
 
-<?php
- include('footer.php') 
-?>
+               <?php
+               include('footer.php')
+               ?>
 
 </main>
