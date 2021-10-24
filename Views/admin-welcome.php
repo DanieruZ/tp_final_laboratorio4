1aac5268f1863ddd81eb318800d41f0f1790bb84
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-10-24 18:20:48 -0300

    fix admin-welcome

--- Views/admin-welcome.php
+++ Views/admin-welcome.php
@@ -1,22 +1,57 @@
-<?php 
+<?php
 
-namespace Views; 
+namespace Views;
+
+use Models\Admin as Admin;
+use DAO\AdminDAO as AdminDAO;
 
 /**
-* * aca lo mismo que en student-welcome pero con el model de admin
-*/
+ * ! esto no funca
+ * * aca datos del estudiante & estado academico 
+ * * el model AcademicStatus se tiene que crear en Models
+ */
+
+$adminRepo = new AdminDAO();
+
+$adminList = $adminRepo->getAllAdmin();
+//$email = "wlorant1@sbwire.com";
+
+//$admin = $adminRepo->getAdminByEmail($email);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-</head>
-<body>
-    <h1>aca datos del admin</h1>
-</body>
-</html>
+<main class="py-5">
+    <section id="listado" class="mb-5">
+        <div class="container-fluid">
+            <title><?php echo $admin->getFirstName() . " " . $admin->getLastName(); ?></title>
+
+            </head>
+
+            <body>               
+                <h1><?php echo $admin->getFirstName() . " " . $admin->getLastName(); ?></h1>
+            </body>
+            <table class="table bg-light">
+                <thead class="bg-dark text-white">
+                    <tr>
+                        <th>Admin ID</th>                       
+                        <th>First Name</th>
+                        <th>Last Name</th>  
+                        <th>Email</th>                       
+                        <th>Active</th>
+                    </tr>     
+
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo $admin->getAdminId(); ?></td>                       
+                        <td><?php echo $admin->getFirstName(); ?></td>
+                        <td><?php echo $admin->getLastName(); ?></td>                      
+                        <td><?php echo $admin->getEmail(); ?></td>                    
+                        <td><?php echo $admin->getActive(); ?></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </section>
+</main>
