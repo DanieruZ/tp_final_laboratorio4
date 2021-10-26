5c9164ef2d78af4450488cdea33fa6dc7bafffdb
DanieruZ <[danieruzeta@gmail.com]>
2021-10-26 16:36:58 -0300

    fixed navs admin & sudent

--- Views/nav-admin.php
+++ Views/nav-admin.php
@@ -5,9 +5,9 @@
   <ul class="navbar-nav ml-auto">
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Admin">Home</a> </li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowAddView">Add Company</a> </li>
-    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowAdminListView" >Company's List</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowCompanyListAdminView" >Company's List</a> </li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Add Student</a> </li>
-    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAdminListView">Student's List</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowStudentListAdminView">Student's List</a> </li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Login/Logout">Logout</a> </li>  
   </ul>
 </nav>
--- Views/nav-student.php
+++ Views/nav-student.php
@@ -4,7 +4,7 @@
   </span>
   <ul class="navbar-nav ml-auto">
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student">Home</a> </li>
-    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowStudentListView">Company's List</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowCompanyListStudentView">Company's List</a> </li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowStudentListView">Student's List</a> </li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Login/Logout">Logout</a> </li>  
   </ul>
