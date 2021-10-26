c6a74dd6b0d8091529ae71eda40e3fcfe4f2c298
DanieruZ <[danieruzeta@gmail.com]>
2021-10-26 16:41:19 -0300

    add check session & minor fixes

--- Controllers/StudentController.php
+++ Controllers/StudentController.php
@@ -4,9 +4,9 @@ namespace Controllers;
 
 use DAO\StudentDAO as StudentDAO;
 use Models\Student;
+use Utils\Utils;
 
-class StudentController
-{
+class StudentController {
 
   private $studentDAO;
 
@@ -15,48 +15,48 @@ class StudentController
     $this->studentDAO = new StudentDAO();
   }
 
-  public function ShowStudentWelcomeView($student)
-  {
+  public function ShowStudentWelcomeView($student) {
+    Utils::checkStudentSession();
     require_once(VIEWS_PATH . "nav-student.php");
     require_once(VIEWS_PATH . "student-welcome.php");
   }
 
-  public function ShowAddView()
-  {
+  public function ShowAddView() {
+    Utils::checkAdminSession();
     require_once(VIEWS_PATH . "nav-admin.php");
     require_once(VIEWS_PATH . "student-add.php");
   }
 
-  public function ShowAdminListView()
-  {
+  public function ShowStudentListAdminView() {
+    Utils::checkAdminSession();
     require_once(VIEWS_PATH . "nav-admin.php");
     $studentList = $this->studentDAO->getAllStudent();
     require_once(VIEWS_PATH . "student-list-admin.php");
   }
 
-  public function ShowStudentListView()
-  {
+  public function ShowStudentListView() {
+    Utils::checkStudentSession();
     require_once(VIEWS_PATH . "nav-student.php");
     $studentList = $this->studentDAO->getAllStudent();
     require_once(VIEWS_PATH . "student-list.php");
   }
 
-  public function AddStudent($careerId,$firstName,$lastName,$dni,$fileNumber,$gender,$birthDate,$email,$phoneNumber,$active)
-  {
+  public function AddStudent($careerId,$firstName,$lastName,$dni,$fileNumber,$gender,$birthDate,$email,$phoneNumber,$active) {
+    Utils::checkAdminSession();
     $studentId = $this->studentDAO->getNextId();
     $student = new Student($studentId,$careerId,$firstName,$lastName,$dni,$fileNumber,$gender,$birthDate,$email,$phoneNumber,$active);
     $this->studentDAO->addStudent($student);
     $this->ShowAddView();
   }
 
-  public function DeleteStudent($studentId)
-  {
+  public function DeleteStudent($studentId) {
+    Utils::checkAdminSession();
     $this->studentDAO->deleteStudentById($studentId);
-    $this->ShowAdminListView();
+    $this->ShowStudentListAdminView();
   }
 
-  public function Index()
-  {
+  public function Index() {
+    Utils::checkStudentSession();
     $student = $_SESSION["student"];
     $title = $student->getFirstName();      
     $this->ShowStudentWelcomeView($student);
