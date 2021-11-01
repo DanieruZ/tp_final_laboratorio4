80ae9c087a2829bca3d1cd4a33f15c504aeb1134
DanieruZ <[danieruzeta@gmail.com]>
2021-11-01 13:37:17 -0300

    fix Config & Request

--- Config/Config.php
+++ Config/Config.php
@@ -1,13 +1,17 @@
-<?php namespace Config;
+<?php 
+
+namespace Config;
 
 define("ROOT", dirname(__DIR__) . "/");
-// IMPORTANT Path to your project's root folder
-define("FRONT_ROOT", "/tp_final_laboratorio4/");
+//Path to your project's root folder
+define("FRONT_ROOT", "/tp_final_laboratotio4/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
-// student's api key
-define('API_KEY','4f3bceed-50ba-4461-a910-518598664c08');
-define('API_URL','https://utn-students-api.herokuapp.com/api/Student');
-?>
 
+define("DB_HOST", "localhost");
+define("DB_NAME", "lab4");
+define("DB_USER", "admin");
+define("DB_PASS", "");
+
+?>
