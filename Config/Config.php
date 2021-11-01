ab5b65db8e485d12e7486283d0f6a5346bd80ee8
DanieruZ <[danieruzeta@gmail.com]>
2021-11-01 19:27:44 -0300

    fix api Config

--- Config/Config.php
+++ Config/Config.php
@@ -9,6 +9,10 @@ define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
 
+// student's api key
+define('API_KEY','4f3bceed-50ba-4461-a910-518598664c08');
+define('API_URL','https://utn-students-api.herokuapp.com/api/Student');
+
 define("DB_HOST", "localhost");
 define("DB_NAME", "lab4");
 define("DB_USER", "admin");
