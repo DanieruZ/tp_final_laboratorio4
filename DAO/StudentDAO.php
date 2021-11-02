c723d24c8bfe19047d7176a306fecdc491de7e36
DanieruZ <[danieruzeta@gmail.com]>
2021-11-02 13:19:47 -0300

    add get & retrieve data students api consume

--- DAO/StudentDAO.php
+++ DAO/StudentDAO.php
@@ -1,115 +1,68 @@
 <?php
 
-    namespace DAO;
+namespace DAO;
     
-    use DAO\IStudentDAO as IStudentDAO;
-    use Models\Student as Student;
-    
-    class StudentDAO implements IStudentDAO {
-    
-      private $studentList = array();
-    
-      public function addStudent(Student $student) {
-        $this->retrieveData();
-        $student->setStudentId($this->getNextId()); 
-        array_push($this->studentList, $student);
-        $this->saveData();
-      }
-    
-      public function getNextId() {
-        $id = 0;
-    
-        foreach($this->studentList as $student) {
-            $id = ($student->getStudentId() > $id) ? $student->getStudentId() : $id;            
-        }
-        return $id + 1;
-    }
-
-      public function getAllStudent() {
-        $this->retrieveData();
-        return $this->studentList;
-      }
-    
-      public function getStudentByEmail($email) {
-        $this->retrieveData();
-        $targetStudent = null;
-    
-        foreach($this->studentList as $student) {
-          if($student->getEmail() == $email) {
-            $targetStudent = $student;
-          }
-        }
-        return $targetStudent;
-      }
-
-      public function deleteStudentById($studentId) {
-        
-        $this->retrieveData();
-        $newList = array();
-        
-       
-        foreach($this->studentList as $student) {
-          if($student->getStudentId() != $studentId) {
-            array_push($newList, $student);
-            
-          }
-        }
-       
-    
-        $this->studentList = $newList;
-        $this->saveData();
-      }
-      
-
-      private function saveData() {
-        $arrayToEncode = array();
-    
-        foreach($this->studentList as $student){
-          $valuesArray["studentId"] = $student->getStudentId();
-          $valuesArray["careerId"] = $student->getCareerId();
-          $valuesArray["firstName"] = $student->getFirstName();
-          $valuesArray["lastName"] = $student->getLastName();
-          $valuesArray["dni"] = $student->getDni();
-          $valuesArray["fileNumber"] = $student->getFileNumber();
-          $valuesArray["gender"] = $student->getGender();
-          $valuesArray["birthDate"] = $student->getBirthDate();
-          $valuesArray["email"] = $student->getEmail();
-          $valuesArray["phoneNumber"] = $student->getPhoneNumber();
-          $valuesArray["active"] = $student->getActive();
-    
-          array_push($arrayToEncode, $valuesArray);
-        }
-    
-        $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
-        file_put_contents('Data/students.json', $jsonContent);
-      }
+use DAO\IStudentDAO as IStudentDAO;
+use Models\Student as Student;
 
     
-      private function retrieveData() {
-        $this->studentList = array();
+class StudentDAO implements IStudentDAO {
     
-        if(file_exists('Data/students.json')) {
-          $jsonContent = file_get_contents('Data/students.json');
-          $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
+  private $studentList = array();
+
+  public function addStudent(Student $student) {
+
+  }
+
+
+  //* Muestra todos los estudiantes consumiendo la API 
+  public function getAllStudent() {
+    $this->retrieveData();
+    return $this->studentList;
+  }
+
+
+  public function getStudentByEmail($email) {
+    $this->retrieveData();
+    $targetStudent = null;
     
-          foreach($arrayToDecode as $valuesArray) {
-            $student = new Student(
-            $valuesArray["studentId"], 
-            $valuesArray["careerId"], 
-            $valuesArray["firstName"], 
-            $valuesArray["lastName"], 
-            $valuesArray["dni"], 
-            $valuesArray["fileNumber"], 
-            $valuesArray["gender"], 
-            $valuesArray["birthDate"], 
-            $valuesArray["email"], 
-            $valuesArray["phoneNumber"], 
-            $valuesArray["active"]);
-    
-            array_push($this->studentList, $student);
-          }
-        }
+    foreach($this->studentList as $student) {
+      if($student->getEmail() == $email) {
+        $targetStudent = $student;
       }
     }
+    return $targetStudent;
+  }
+
+  //* Consume la API de students
+  private function retrieveData() {
+    $this->studentList = array();
     
-    ?> 
+    $apiStudent = curl_init(API_URL . 'Student');
+    curl_setopt($apiStudent, CURLOPT_URL, API_URL);
+    curl_setopt($apiStudent, CURLOPT_HTTPHEADER, array('x-api-key: ' . API_KEY));
+    curl_setopt($apiStudent, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($apiStudent);
+    $arrayToDecode = ($response) ? json_decode($response, true) : array();
+
+    foreach ($arrayToDecode as $valuesArray) {
+      $student = new Student(
+        $valuesArray["studentId"],
+        $valuesArray["careerId"],
+        $valuesArray["firstName"],
+        $valuesArray["lastName"],
+        $valuesArray["dni"],
+        $valuesArray["fileNumber"],
+        $valuesArray["gender"],
+        $valuesArray["birthDate"],
+        $valuesArray["email"],
+        $valuesArray["phoneNumber"],
+        $valuesArray["active"]
+      );
+      array_push($this->studentList, $student);
+    }
+  }
+
+}
+    
+?> 
