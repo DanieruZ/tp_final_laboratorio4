2b740f3dcc5927c0ec7f3a5c2cf76993bf87f183
DanieruZ <[danieruzeta@gmail.com]>
2021-11-02 13:58:30 -0300

    add addStudent query

--- DAO/StudentDAO.php
+++ DAO/StudentDAO.php
@@ -4,14 +4,46 @@ namespace DAO;
     
 use DAO\IStudentDAO as IStudentDAO;
 use Models\Student as Student;
-
+use DAO\Connection as Connection;
     
 class StudentDAO implements IStudentDAO {
     
   private $studentList = array();
+  private $connection;
 
   public function addStudent(Student $student) {
+    try {
+      $sql = "INSERT INTO Student (studentId, careerId, firstName, lastName, dni, fileNumber, gender, birthDate, email, phoneNumber, active) 
+              VALUES (:studentId,:careerId,:firstName,:lastName,:dni,:fileNumber,:gender,:birthDate,:email,:phoneNumber,:active)";
 
+      $parameters['studentId'] = $student->getStudentId();
+      $parameters['careerId'] = $student->getCareerId();
+      $parameters['firstName'] = $student->getFirstName();
+      $parameters['lastName'] = $student->getLastName();
+      $parameters['dni'] = $student->getDni();
+      $parameters['fileNumber'] = $student->getFileNumber(); 
+      $parameters['gender'] = $student->getGender();
+      $parameters['birthDate'] = $student->getBirthDate();
+      $parameters['email'] = $student->getEmail();
+      $parameters['phoneNumber'] = $student->getPhoneNumber();
+      $parameters['active'] = $student->getActive();
+      
+      $this->connection = Connection::GetInstance();
+      $this->connection->ExecuteNonQuery($sql,$parameters);
+
+    } catch (\PDOException $ex) {
+        throw $ex;
+      }
+  }
+
+
+  public function getNextId() {
+    $id = 0;
+
+    foreach($this->studentList as $student) {
+        $id = ($student->getStudentId() > $id) ? $student->getStudentId() : $id;            
+    }
+    return $id + 1;
   }
 
 
