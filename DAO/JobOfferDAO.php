1138c9884152e274a2de4fb8fa2bbcf83b8f1437
JeremiasRoca <jereroca2@gmail.com>
2021-11-04 17:28:44 -0300

    add getAllJobOffer

--- DAO/JobOfferDAO.php
+++ DAO/JobOfferDAO.php
@@ -10,12 +10,14 @@ class JobOfferDAO implements IJobOfferDAO {
 
   private $jobOfferList = array();
   private $connection;
+  private $tableName = 'joboffer';
+ 
 
   public function addJobOffer(JobOffer $jobOffer) {
  
     try {
 
-      $sql = "INSERT INTO  jobOffer (companyId, jobPositionId, studentId, adminId, description, active) 
+      $sql = "INSERT INTO  joboffer (companyId, jobPositionId, studentId, adminId, description, active) 
                 VALUES ( :companyId, :jobPositionId, :studentId, :adminId, :description, :active);";
 
     
@@ -36,12 +38,47 @@ class JobOfferDAO implements IJobOfferDAO {
     }
   }
 
-
- 
-  function getAllJobOffer(){
-
+  public function getNextId()
+  {
+    $id = 0;
+    foreach ($this->jobOfferList as $jobOffer) {
+      $id = ($jobOffer->getJobOfferId() > $id) ? $jobOffer->getJobOfferId() : $id;
+    }
+    return $id + 1;
   }
+ 
+ 
 
+  public function getAllJobOffer()
+  {
+    try {
+      $jobOfferList = array();
+
+      $sql = "SELECT * FROM " . $this->tableName;
+
+      $this->connection = Connection::GetInstance();
+
+      $toJobOffer = $this->connection->Execute($sql);
+
+      foreach ($toJobOffer as $row) {
+
+        $jobOffer = new JobOffer();
+        $jobOffer->setJobOfferId($row["jobOfferId"]);
+        $jobOffer->setCompanyId($row["companyId"]);
+        $jobOffer->setJobPositionId($row["jobPositionId"]);
+        $jobOffer->setStudentId($row["studentId"]);
+        $jobOffer->setAdminId($row["adminId"]);
+        $jobOffer->setDescription($row["description"]);
+        $jobOffer->setActive($row["active"]);
+
+        array_push($jobOfferList, $jobOffer);
+      }
+
+      return $jobOfferList;
+    } catch (\PDOException $ex) {
+      throw $ex;
+    }
+  }
 }
 
 
