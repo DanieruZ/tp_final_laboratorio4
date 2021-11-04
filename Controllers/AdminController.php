44b8c5a53e8c893107ac773461d66bb95d83cd48
JeremiasRoca <jereroca2@gmail.com>
2021-11-03 22:27:43 -0300

    add addAdmin

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace Controllers;
 
 use DAO\AdminDAO as AdminDAO;
+use Models\Admin as Admin;
+use Utils\Utils;
 
 class AdminController {
         
@@ -17,10 +19,30 @@ class AdminController {
     require_once(VIEWS_PATH."admin-welcome.php");
   }
 
+  public function ShowAddAdminView(){
+    require_once(VIEWS_PATH."nav-admin.php");
+    require_once(VIEWS_PATH."admin-add.php");
+}
+
+
   public function Index() {
     $admin = $_SESSION["admin"];
     $title = $admin->getFirstName();   
     $this->ShowAdminWelcomeView($admin);
+  }
+
+  public function AddAdmin($firstName, $lastName,$dni,$email,$active) {  
+    Utils::checkAdminSession();    
+    $adminId = $this->AdminDAO->getNextId(); //genera el siguiente ID
+    $admin = new Admin();   
+    $admin->setFirstName($firstName);
+    $admin->setLastName($lastName);
+    $admin->setDni($dni);
+    $admin->setEmail($email);
+    $admin->setActive($active); 
+
+    $this->AdminDAO->addAdmin($admin);   
+    $this->ShowAddAdminView();
   }
 
 }
