9a033f28b5a2d99c0ec1a4f5321f7ae3e0841557
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-11-05 18:04:06 -0300

    update nav student

--- Views/nav-student.php
+++ Views/nav-student.php
@@ -5,7 +5,8 @@
   <ul class="navbar-nav ml-auto">
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student">Home</a> </li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company\ShowCompanyListStudentView">Company's List</a> </li>
-    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>">St</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>JobOffer\ShowApplicationView"> application</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>JobOffer\ShowApplicationView"> History application</a> </li>
     <li class="nav-item"> <a class="nav-link" href="<?php echo FRONT_ROOT ?>Login/Logout">Logout</a> </li>  
   </ul>
 </nav>
