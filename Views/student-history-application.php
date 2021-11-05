449c084d88d2b8b3b3f855e11eb05784f9d88bcd
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-11-05 19:17:19 -0300

    fix history application

--- Views/student-history-application.php
+++ Views/student-history-application.php
@@ -1,53 +1,41 @@
 <main class="py-5">
     <section id="listado" class="mb-5">
         <div class="container-fluid">
-            <h2 class="mb-4">JobOffer List</h2>
+            <h2 class="mb-4">History Application</h2>
             <table class="table bg-light">
-                <thead class="bg-dark text-white">                  
+                <thead class="bg-dark text-white">
                     <th>companyName</th>
-                    <th>Job Position</th>                  
-                    <th>description</th>             
+                    <th>Job Position</th>
+                    <th>description</th>
                     <th>active</th>
-                    <th>Application</th>                   
+                    <th>Application</th>
 
                 </thead>
                 <tbody>
                     <?php
-                   
-                        foreach ($jobOfferList as $jobOffer) {
+
+
+
+                    foreach ($jobApplicationList as $jobApplication) {
                     ?>
-                    <tr>
-                    <td><?php echo $jobOfferId = $jobOffer->getJobOfferId(); ?></td>
-                    <td><?php echo $jobOffer->getCompanyId(); ?></td>
-                        <td><?php echo $jobOffer->getCompanyName(); ?></td>
-                        <td><?php echo $jobOffer->getJobPositionId(); ?></td>
-                        <td><?php echo $jobOffer->getStudentId(); ?></td>
-                        <td><?php echo $jobOffer->getDescription(); ?></td>
-                        <td><?php echo $jobOffer->getAdminId(); ?></td>                       
-                        <td><?php echo $jobOffer->getActive(); ?></td>                         
-                      
+                        <td><?php echo $jobApplication->getJobApplicationId(); ?></td>
+                        <td><?php echo $jobApplication->getCompanyId(); ?></td>
+                        <td><?php echo $jobApplication->getStudentId(); ?></td>
+                        <td><?php echo $jobApplication->getActive(); ?></td>
+
                         <td>
-                            <button type="submit" name="btnRemove" class="btn btn-danger">
-                                <a href="<?php if (isset($jobOffer)) {
-                                    echo FRONT_ROOT . "JobOffer/DeleteJobOffer/" . $jobOffer->getJobOfferId();}; ?>">Delete</a>
-                            </button>                            
-                        
-                       
+
                             <button type="submit" name="btnChange" class="btn btn-danger">
                                 <a href="<?php if (isset($jobOffer)) {
-                                                        echo FRONT_ROOT . "JobOffer/changeJobOfferActiveById/" . $jobOffer->getJobOfferId();}; ?>">Active</a>
-                            </button>                            
-                        
-                       
-                            <button type="submit" name="btnChange" class="btn btn-danger">
-                                <a href="<?php if (isset($jobOffer)) {
-                                    echo FRONT_ROOT . "JobOffer/changeJobOfferInactiveById/" . $jobOffer->getJobOfferId();}; ?>">Inactive</a>
-                            </button>                            
+                                                echo FRONT_ROOT . "JobOffer/changeJobOfferInactiveById/" . $jobOffer->getJobOfferId();
+                                            }; ?>">Inactive</a>
+                            </button>
                         </td>
-                    </tr>
+                        </tr>
                     <?php
-                        }
-                        ?>
+
+                    }
+                    ?>
                 </tbody>
 
             </table>
