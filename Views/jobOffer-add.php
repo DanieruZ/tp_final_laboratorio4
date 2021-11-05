19eaf9dfbad8eb24a09806fc603e4b93a063fe4c
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-11-05 18:04:32 -0300

    fix job offer add

--- Views/jobOffer-add.php
+++ Views/jobOffer-add.php
@@ -1,28 +1,53 @@
 <?php
-require_once "nav-admin.php";
+
 ?>
-<head>
-     <title>New Offer</title>
-</head>
+
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
                <h2 class="mb-4">Add Offer</h2>
                <form action="<?php echo FRONT_ROOT ?>JobOffer/AddJobOffer" method="post" class="bg-light p-5">
-                    <div class="col">                         
+                    <div class="col">
                          <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="description">Description</label>
-                                   <input type="text" name="description" value="" class="form-control" Required>
+                              <div class="form-group">                             
+                                   <select name="companyId" name= "companyName" required class="form-control form-control-ml">
+                                        <option style="color:grey" hidden selected>Company</option>
+                                        <?php
+                                       
+                                        if (isset($companyList)) {
+                                             foreach ($companyList as $company) {
+                                                  echo "<option value=" . $company->getCompanyId() . ">" . $company->getCompanyName() . "</option>";
+                                             }
+                                        }
+                                        ?>
+                                   </select>
                               </div>
+                              
+                         </div>
+                         <div class="form-group">                                      
+                              <select name="jobPositionId" class="form-control form-control-ml">
+                                   <option hidden selected style="color:gray">Position</option>
+
+                                   <?php
+                                   if (isset($jobPositionList)) {
+                                        foreach ($jobPositionList as $jobPosition) {
+                                             echo "<option value=" . $jobPosition->getJobPositionId() . ">" . $jobPosition->getDescription() . "</option>";
+                                        }
+                                   }
+                                   ?>
+                              </select>
+                         </div>
+                         <div class="form-group">
+                              <textarea type="text" name="description" maxlength="200" class="form-control form-control-ml" required value="" placeholder="Descripción"></textarea>
                          </div>
                          <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="NameCompany">Name Company</label>
-                                   <input type="text" name="NameCompany" value="" class="form-control" Required>
-                              </div>
                          </div>
-                         
+
+
+
+
+
+
                     </div>
                     <button type="submit" class="btn btn-dark ml-auto d-block">Add</button>
                     <a href="<?php echo FRONT_ROOT ?>Admin">Back to Main</a>
