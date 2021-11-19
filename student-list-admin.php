cf67991f2fb0ebdd1cf1191bef481a0db304c5a5
Daniel Zarategui <66543074+DanieruZ@users.noreply.github.com>
2021-11-19 19:05:24 -0300

    fix student-list-admin

---
+++ student-list-admin.php
@@ -0,0 +1,52 @@
+<main class="py-5">
+   <section id="listado" class="mb-5">
+	   <div class="container-fluid">
+		   <h2 class="mb-4">Student's List</h2>
+
+		   <table class="table bg-light">
+			   <thead class="bg-primary text-white">
+				   <th>Career ID</th>
+				   <th>First Name</th>
+				   <th>Last Name</th>
+				   <th>DNI</th>
+				   <th>File Number</th>
+				   <th>Gender</th>
+				   <th>Birthdate</th>
+				   <th>Email</th>
+				   <th>Phone Number</th>
+				   <th>Active</th>
+				   <th>Options</th>
+			   </thead>
+			   <tbody>
+				 <?php
+						if(isset($studentList)) {
+							foreach ($studentList as $student) {
+					?>
+						<tr>
+							<td><?php echo $student->getCareerId(); ?></td>
+							<td><?php echo $student->getFirstName(); ?></td>
+							<td><?php echo $student->getLastName(); ?></td>
+							<td><?php echo $student->getDni(); ?></td>
+							<td><?php echo $student->getFileNumber(); ?></td>
+							<td><?php echo $student->getGender(); ?></td>
+							<td><?php echo $student->getBirthDate(); ?></td>
+							<td><?php echo $student->getEmail(); ?></td>
+							<td><?php echo $student->getPhoneNumber(); ?></td>
+							<td><?php echo $student->getActive(); ?></td>
+							<td>
+						  	<button type="submit" name="btnRemove"  class="btn btn-outline-danger">
+									<a href= "<?php if(isset($student)) {echo FRONT_ROOT . "Student/DeleteStudent/" . $student->getStudentId();}; ?>">Delete</a>
+            		</button>
+							</td>
+						</tr>
+					  <?php
+						   }
+					   }
+					  ?>
+				  
+			   </tbody>
+		   </table>
+	   </div>
+   </section>
+</main>
+<?php include('footer.php') ?> 
