30e5073a92636bd7adba8cbca437be86db212469
Jeremias Roca <jereroca2@gmail.com>
2021-11-19 19:25:15 -0300

    Fix ICompanyDAO

---
+++ ICompanyDAO.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DAO;
+
+use Models\Company as Company;
+
+interface ICompanyDAO {
+
+  function addCompany(Company $company);
+  function getAllCompany();
+  function getCompanyById($companyId);
+  function getCompanyByName($companyName);
+  function deleteCompanyById($companyId);
+  function updateCompany(Company $company);
+  
+}
+
+?> 
