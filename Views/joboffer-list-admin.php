8d748a67bc998242ace578cbd96cc7ca1487b4a8
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-11-19 18:14:30 -0300

    fix JobOffer List admin

--- Views/joboffer-list-admin.php
+++ Views/joboffer-list-admin.php
@@ -1,57 +1,80 @@
+<?php
+
+namespace Views;
+
+use Models\Student as Student;
+use DAO\StudentDAO as StudentDAO;
+
+$studentRepo = new StudentDAO();
+$studentList = $studentRepo->getAllStudent();
+
+
+?>
+
+
 <main class="py-5">
     <section id="listado" class="mb-5">
         <div class="container-fluid">
-            <h2 class="mb-4">JobOffer List</h2>
+            <h2 class="mb-4">JobOffer's List</h2>
             <table class="table bg-light">
-                <thead class="bg-dark text-white">
-                    <th>jobOfferId</th>
-                    <th>companyId</th>
-                    <th>companyName</th>
-                    <th>jobPositionId</th>
-                    <th>studentId</th>
-                    <th>description</th>
-                    <th>adminId</th>
-                    <th>active</th>
-                    <th>Change Status</th>                   
+                <thead class="bg-primary text-white">
+                    <th>Company Name</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th>Options</th>
 
                 </thead>
                 <tbody>
                     <?php
-                   
-                        foreach ($jobOfferList as $jobOffer) {
+
+                    foreach ($jobOfferList as $jobOffer) {                     
+
+                        
+                                if (($jobOffer->getActive()))
+                                    $jobOffer->setActive("Enable");
+                                else
+                                    $jobOffer->setActive("Disable");
+
                     ?>
-                    <tr>
-                    <td><?php echo $jobOfferId = $jobOffer->getJobOfferId(); ?></td>
-                    <td><?php echo $jobOffer->getCompanyId(); ?></td>
-                        <td><?php echo $jobOffer->getCompanyName(); ?></td>
-                        <td><?php echo $jobOffer->getJobPositionId(); ?></td>
-                        <td><?php echo $jobOffer->getStudentId(); ?></td>
-                        <td><?php echo $jobOffer->getDescriptionJobOffer(); ?></td>
-                        <td><?php echo $jobOffer->getAdminId(); ?></td>                       
-                        <td><?php echo $jobOffer->getActive(); ?></td>                         
-                      
-                        <td>
-                            <button type="submit" name="btnRemove" class="btn btn-danger">
-                                <a href="<?php if (isset($jobOffer)) {
-                                    echo FRONT_ROOT . "JobOffer/DeleteJobOffer/" . $jobOffer->getJobOfferId();}; ?>">Delete</a>
-                            </button>                            
-                        
-                       
-                            <button type="submit" name="btnChange" class="btn btn-danger">
-                                <a href="<?php if (isset($jobOffer)) {
-                                                        echo FRONT_ROOT . "JobOffer/changeJobOfferActiveById/" . $jobOffer->getJobOfferId();}; ?>">Active</a>
-                            </button>                            
-                        
-                       
-                            <button type="submit" name="btnChange" class="btn btn-danger">
-                                <a href="<?php if (isset($jobOffer)) {
-                                    echo FRONT_ROOT . "JobOffer/changeJobOfferInactiveById/" . $jobOffer->getJobOfferId();}; ?>">Inactive</a>
-                            </button>                            
-                        </td>
-                    </tr>
+                                <tr>
+                                    <td><?php echo $jobOffer->getCompanyName(); ?></td>
+                                    <td><?php echo $jobOffer->getJobOffer_description(); ?></td>
+                                    <td><?php echo $jobOffer->getActive(); ?></td>
+
+                                    <td>
+                                        <button type="submit" name="btnRemove" class="btn btn-outline-danger">
+                                            <a href="<?php if (isset($jobOffer)) {
+                                                            echo FRONT_ROOT . "JobOffer/DeleteJobOffer/" . $jobOffer->getId_jobOffer();
+                                                        }; ?>">Delete</a>
+                                        </button>
+
+
+                                        <button type="submit" name="btnChange" class="btn btn-outline-success">
+                                            <a href="<?php if (isset($jobOffer)) {
+                                                            echo FRONT_ROOT . "JobOffer/changeJobOfferActiveById/" . $jobOffer->getId_jobOffer();
+                                                        }; ?>">Enabled</a>
+                                        </button>
+
+
+                                        <button type="submit" name="btnChange" class="btn btn-outline-warning">
+                                            <a href="<?php if (isset($jobOffer)) {
+                                                            echo FRONT_ROOT . "JobOffer/changeJobOfferInactiveById/" . $jobOffer->getId_jobOffer();
+                                                        }; ?>">Disabled</a>
+                                        </button>
+
+                                        <button type="submit" name="btnUpdate" class="btn btn-outline-primary">
+                                            <a href="<?php if (isset($jobOffer)) {
+                                                            echo FRONT_ROOT . "JobOffer/ShowJobOfferListUpdateAdminView/" . $jobOffer->getId_jobOffer();
+                                                        }; ?>">Update</a>
+                                        </button>
+                                    </td>
+                                </tr>
                     <?php
-                        }
-                        ?>
+                            }
+                        
+                    
+
+                    ?>
                 </tbody>
 
             </table>
